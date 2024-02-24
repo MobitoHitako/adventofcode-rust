5f8d78967564fafeb45dbf7ecc063992b8fd9fac
MobitoHitako <shahzebmughel@hotmail.com>
2024-02-24 20:09:32 +0100

    Started Day03 of Advent of Code 2015

---
+++ src/adv2015/day03.rs
@@ -0,0 +1,4 @@
+pub fn run(){
+    println!("Welcome to day03!");
+
+}
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ mod adv2015;
 
 fn adv2015(){
     // adv2015::day01::run(true);
-    adv2015::day02::run(true);
+    // adv2015::day02::run(true);
+    adv2015::day03::run();
 }
 
 fn adv2022(){
